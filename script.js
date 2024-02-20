aadef2c401acad261683842e9121d5d94e8fc7a2
MarcelloConti <92038350+MarcelloConti@users.noreply.github.com>
2024-02-20 15:42:52 -0300

    Added Js code to the script.js file ``` Changed the responsive design of the website ``` Made slight changes to the index.html file ```

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+function scrollToElement(elementSelector) {  
+    const element = document.querySelector(elementSelector);
 
+    if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const link1 = document.getElementById("link1");
+    const link2 = document.getElementById("link2");
+    const link3 = document.getElementById("link3");
+
+    link1.addEventListener('click', () => {
+        scrollToElement('.about');
+    });
+
+    link2.addEventListener('click', () => {
+        scrollToElement('.experiences');
+    });
+
+    link3.addEventListener('click', () => {
+        scrollToElement('.box');
+    });
+});
